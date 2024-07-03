b25f38621c446daa13d00717d30ca63b492a9058
Noah Humphrey <thatnoahlh@gmail.com>
2024-07-03 18:21:46 +0000

    js and bg updates

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ $( document ).ready(function() {
         window.alert("DID YOU JUST CLICK ME?!");
     });
 });
+
+function setup(){
+    createCanvas(500,300);
+    background(145,7,132);
+    circle(width/2, height/2,50);
+
+}
+
+function draw(){
+    
+}
